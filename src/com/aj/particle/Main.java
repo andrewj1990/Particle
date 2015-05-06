53efb293fa1a5a1d54f38defe62bb73456371756
Andrew Jackson <asj@uvic.ca>
2015-05-05 20:27:49 -0700

    fixed typo

--- src/com/aj/particle/Main.java
+++ src/com/aj/particle/Main.java
@@ -148,7 +148,7 @@ public class Main extends Canvas implements Runnable {
 		g.drawString("Particles : " + screen.getParticlesSize(), 10, 20);
 		g.drawString("Emitted Particles : " + screen.getEmittedParticles() + " (up, down)", 10, 35);
 //		g.drawString("Particle Splits : " + screen.getParticleSplits(), 10, 50);
-		g.drawString("Right click to add particles | Left click to add vacuum hole", 10, 50);	
+		g.drawString("Left click to add particles | Right click to add vacuum hole", 10, 50);	
 		g.drawString("Spacebar to clear", 10, 65);	
 		
 		g.dispose();
